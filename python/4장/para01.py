a7e44e982eb3563ae1bc545f5dc8b782aa394b26
JeonHoe <wjsgh7289@gmail.com>
2022-07-02 11:01:35 +0900

    unit4 para01 modify

---
+++ python/4장/para01.py
@@ -0,0 +1,24 @@
+# 세미클론
+print('hello'); print('1234')
+
+# 한 줄 주석
+print('hello, world!')
+# print('hello World!')
+a = 1 + 2 # 더하기
+
+# 블록 주석
+# print('hello, world!')
+# print('1234567890')
+
+# 들여쓰기
+a = 10
+if a == 10:
+# print('10입니다.') --> 들여쓰기 문법 에러
+    print('10입니다.') # 들여쓰기 문법 준수
+
+# 코드 블록
+if a == 10:
+    # print('10')
+    #     print('입니다') --> 코드 블록 에러
+    print('10')
+    print('입니다') # 코드 블록 준수
